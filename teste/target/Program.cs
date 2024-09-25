df411ec060e243cc0389a034aed908a69f716777
Alessandro Barreto <albarretoa@gmail.com>
2024-09-25 12:57:41 -0300

    Add files via upload

---
+++ teste/target/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ContadordeLetras
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a palavra: ");
+        string palavra = Console.ReadLine();
+
+        int count = ContadorDeLetras(palavra, 'a');
+
+        if (count > 0)
+        {
+            Console.WriteLine($"A letra 'a' aparece {count} vezes na palavra.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' não aparece na palavra.");
+        }
+    }
+
+    static int ContadorDeLetras(string palavra, char letter)
+    {
+        int count = 0;
+        foreach (char c in palavra.ToLower())
+        {
+            if (c == letter || c == char.ToUpper(letter))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
